39d93d41edca8ec9e35cbc5fcc688363ff1907a6
zinkop <99264930+zinkop@users.noreply.github.com>
2022-09-26 20:24:34 +0100

    Update 2-puppet_custom_http_response-header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,19 +1,8 @@
-#puppet advance
-exec { 'update':
-  command  => 'sudo apt-get update',
-  provider => shell,
-}
--> package {'nginx':
-  ensure => present,
-}
--> file_line { 'header line':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  line   => "	location / {
-  add_header X-Served-By ${hostname};",
-  match  => '^\tlocation / {',
-}
--> exec { 'restart service':
-  command  => 'sudo service nginx restart',
+# Automation: creates a custom HTTP header response with Puppet.
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
   provider => shell,
 }
